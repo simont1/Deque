6cbb7f03aff1c591ff9106fcb5036d7c4f959609
rlin6 <rlin6@stuy.edu>
2018-04-25 23:40:41 -0400

    changes based on review

--- QQKachoo.java
+++ QQKachoo.java
@@ -1,86 +1,133 @@
+//Square - Ricky Lin, Kyle Tau, Simon Tsui
+//APCS2 pd02
+//Lab 02
+
 public class QQKachoo<Card> implements Deque<Card>{
-    DLLNode<Card> _head, _tail;
-    int _size;
-    public QQKachoo(){
-	_head = new DLLNode<Card>(null, null, null);
-	_tail = new DLLNode<Card>(null, null, null);
-	_size = 0;
+
+    DLLNode<Card> _head, _tail;  //pointer to head and tail
+    int _size;  //size of deque
+
+    public QQKachoo() {  //constructor initializes head tail and size
+	     _head = new DLLNode<Card>(null, null, null);
+	     _tail = new DLLNode<Card>(null, null, null);
+	     _size = 0;
     }
-    public int size(){
-	return _size;
+
+    public int size() {  //return size
+	     return _size;
     }
-    public boolean add(Card e){
-	addLast(e);
-	return true;
+
+    public boolean add(Card e) {  //add to the end
+	      addLast(e);
+	      return true;
     }
-    public void addFirst(Card e){
-	if(_size == 0){
-	    _head = new DLLNode<Card>(e, null, null);
-	    _tail = _head;
-	}
-	else{
-	    DLLNode<Card> temp = new DLLNode<Card>(e, null, _head);
-	    _head = temp;
-	}
-	_size++;
+
+    public void addFirst(Card e) {  //add to the front
+	     if ( _size == 0 ) {
+	        _head = new DLLNode<Card>(e, null, null);
+	        _tail = _head;
+	     }
+
+	     else {
+	        DLLNode<Card> temp = new DLLNode<Card>(e, null, _head);
+	        _head = temp;
+	     }
+	     _size++;
     }
-    public void addLast(Card e){
-	if(_size == 0){
-	    _head = new DLLNode<Card>(e, null, null);
-	    _tail = _head;
-	}
-	else{
-	    DLLNode<Card> temp = new DLLNode<Card>(e, _tail, null);
-	    _tail = temp;
-	}
-	_size++;
+
+
+    public void addLast(Card e) {  //add to end
+	     if ( _size == 0 ) {
+	        _head = new DLLNode<Card>(e, null, null);
+	        _tail = _head;
+	     }
+
+	     else {
+	        DLLNode<Card> temp = new DLLNode<Card>(e, _tail, null);
+	        _tail = temp;
+	     }
+	     _size++;
     }
-    public Card element(){
-	return _head.getCargo();
+
+    public Card element() {  //return first element
+	     return _head.getCargo();
     }
-    public Card getFirst(){
-	return element();
+
+    public Card getFirst() {  //return 1st element
+	     return element();
     }
-    public Card getLast(){
-	return _tail.getCargo();
+
+    public Card getLast() {  //return last element
+	     return _tail.getCargo();
     }
-    public Card remove(){
-	Card temp = _head.getCargo();
-	_head = _head.getNext();
-	return temp;
+
+    public Card remove() {  //removes and returns first element
+	     Card temp = _head.getCargo();
+	     _head = _head.getNext();
+	     return temp;
     }
-    public Card removeFirst(){
-	return remove();
+
+    public Card removeFirst() {  //removes and returns first element
+	     return remove();
     }
-    public Card removeLast(){
-	Card temp = _tail.getCargo();
-	_tail = _tail.getPrev();
-	return temp;
+
+    public Card removeLast() {  //removes and returns last element
+	     Card temp = _tail.getCargo();
+	     _tail = _tail.getPrev();
+	     return temp;
     }
-    public boolean contains(Card e){
-	DLLNode temp = _head;
-	for (int i = 0; i < _size; i++) {
-	    if (temp.getCargo().equals(e))
-		return true;
-	    temp = temp.getNext();
-	}
-	return false;
+
+    public boolean contains(Card e) {  //checks if deque has card e
+	     DLLNode temp = _head;
+
+       for ( int i = 0; i < _size; i++ ) {
+
+          if ( temp.getCargo().equals(e) ) {
+		          return true;
+          }
+
+          temp = temp.getNext();
+	     }
+	     return false;
     }
-    public String toString(){
+
+    public String toString() {  //overwrite toString method
     	String retstr = "";
-	DLLNode a = _head;
-     	while(a.getNext() != null){
-     	    retstr+=a.getCargo();
-	    a = a.getNext(); //Doesnt print last statement
-	}
-	return retstr;
+	    DLLNode temp = _head;
+
+      while( temp != null ) {
+     	  retstr += temp.getCargo() + ", ";
+	      temp = temp.getNext();
+	    }
+
+      if ( retStr.length() > 1 ) {
+    		retStr = retStr.substring( 0, retStr.length() - 2 );
+      }
+
+	    return retstr;
     }
 
 
     public static void main(String[] args){
-	QQKachoo<String> basic = new QQKachoo<String>();
-	basic.add("I");
-	basic.addFirst("have");
-	System.out.println(basic);
-    }
+
+	     QQKachoo<String> basic = new QQKachoo<String>();
+	     basic.add("I");
+	     basic.addFirst("have");
+       basic.addLast("done");
+       System.out.println(basic.contains("have")); //expect true
+	     System.out.println(basic);  //expect have I done
+       while(!basic.isEmpty()){
+         System.out.println(basic.getLast())
+     	   System.out.println(basic.removeLast());
+     	}
+
+      basic.add("I");
+      basic.addFirst("have");
+      basic.addLast("done");
+      System.out.println(basic.contains("false")); //expect false
+      while(!basic.isEmpty()){
+        System.out.println(basic.getFirst());
+        System.out.println(basic.removeFirst());
+     }
+   }
 }
